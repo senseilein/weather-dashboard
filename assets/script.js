97a7018fa09dbb80c9b7ed0acb6361a58c217707
Senseilein <73607360+senseilein@users.noreply.github.com>
2023-02-05 19:52:12 +0000

    Implement createCityBtn function that creates a button using user input text

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,10 @@
-// TODO:create Query URL
+const history = $("#history");
+
+// * Create a button with user input text
+function createCityBtn() {
+  const searchInput = $("#search-input").val().trim();
+  const cityBtn = $("<button>");
+  cityBtn.addClass("btn, city-button");
+  cityBtn.text(searchInput);
+  history.append(cityBtn);
+}
