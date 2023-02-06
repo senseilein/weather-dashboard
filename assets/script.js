807fde7331760ff58dc49ca6add8eaf051639e7a
Senseilein <73607360+senseilein@users.noreply.github.com>
2023-02-06 11:15:17 +0000

    ✨ Implement all necessary (basic) functions to call Geocoding API & retrieve lat-lon based on user input city

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,17 @@
 const history = $("#history");
+const form = $("#search-form");
+const submitBtn = $("#search-button");
 
-// ! TODO: Add guard, only create a button for valid city, i.e. the ones that have retrievable coordinates
-/**
+//Array created to store lat and lon extracted from Geocoding API response
+// !needs to be accessible from all the different functions used for that particular API
+let cityCoordinates = [];
+
+// ! TODO: Add defense, only create a button for valid city,
+// i.e. the ones that have retrievable coordinates
+// if input empty inform user accordingly and do nothing
+// remove pattern tag and create function to validate input
+// TODO: TBS issue with Bootstrap class my-5 not rendered when dynamically added to cityBtn
+/*
  * * Create a button with user input text and
  * * Append it to the #history section (inside the aside)#
  */
@@ -14,51 +24,61 @@ function createCityBtn() {
   history.append(cityBtn);
 }
 
-// ! TODO: Improve - if failed inform user accordingly
-
-/**
- * * Retrieve the lat-lon coordinates of the user input city
- * @returns {array} [lat, lon]
- */
-function getCityCoordinates(event) {
-  // this will be a callback function used for input submit button click handler
-  event.preventDefault();
-
+function buildGeocodingQueryURL() {
   // get user input and store it in cityName
   const cityName = $("#search-input").val().trim();
-  console.log({ cityName });
+  console.log("inputed city is " + cityName);
 
-  // create query URL (limit to 1 result)
+  // create query URL(limit to 1 result)
   let geoQueryURL = `http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=1&appid=${apiKey}`;
-  let cityCoordinates = [];
-
-  //call Geocoding API to retrieve city coordinates
-  $.ajax({
-    url: geoQueryURL,
-    method: "GET",
-  })
-    .then(function (response) {
-      console.log("response");
-      console.log(response);
-      console.log("data below");
-      console.log(response[0].lat);
-      console.log(response[0].lon);
-
-      // collect city latitude and longitude and push it the the cityCoordinates array
-      let cityLat = response[0].lat;
-      let cityLon = response[0].lon;
-
-      cityCoordinates.push(cityLat);
-      cityCoordinates.push(cityLon);
-    })
-    .fail(function () {
-      console.log("ERROR ERROR");
-    });
-
-  return cityCoordinates;
+  return geoQueryURL;
 }
 
-// $("#search-button").on("click", function (event) {
-//   createCityBtn();
-//   getCityCoordinates(event);
-// });
+function collectCityCoordinates(response) {
+  console.log(response);
+  console.log(
+    "From Geocoding response /n lat is " +
+      response[0].lat +
+      "/n lon is " +
+      response[0].lon
+  );
+
+  // collect city latitude and longitude and push it to the cityCoordinates array available globally
+  cityCoordinates.push(response[0].lat);
+  cityCoordinates.push(response[0].lon);
+}
+
+// ! TODO: Improve - if failed then inform user accordingly
+/**
+ * * Call the Geocoding API and
+ * * Collect the lat-lon coordinates of the user input city
+ * @returns true for now // will modif later with IF statement depending on API response
+ */
+function getCityCoordinates() {
+  const queryURL = buildGeocodingQueryURL();
+
+  //call Geocoding API to extract city coordinates from the response provided
+  $.ajax({
+    url: queryURL,
+    method: "GET",
+  }).then(function (response) {
+    return collectCityCoordinates(response);
+  });
+  // .fail(function () {
+  //   console.log("ERROR ERROR");
+  //   return false;
+  // });
+
+  console.log(cityCoordinates);
+  return true;
+}
+
+// Event delegation - Handle form submission with submit button
+form.on("submit", submitBtn, function (event) {
+  event.preventDefault();
+
+  createCityBtn();
+  //Make inputed city coordinates available in this scope by storing funtion in the variable
+  let currentCityCoordinates = getCityCoordinates();
+  console.log(currentCityCoordinates);
+});
